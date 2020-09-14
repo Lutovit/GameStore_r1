cbb2093af1aa743af4fc8485e2a97ffbedcde07c
Lutovit <pilot_mig@bk.ru>
2020-09-14 12:47:07 +0300

    Initial commit

---
+++ GameStore.Domain/Migrations/202008071029276_MigrateDB_2.cs
@@ -0,0 +1,20 @@
+﻿namespace GameStore.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MigrateDB_2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Games", "ImageData", c => c.Binary());
+            AddColumn("dbo.Games", "ImageMimeType", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Games", "ImageMimeType");
+            DropColumn("dbo.Games", "ImageData");
+        }
+    }
+}
